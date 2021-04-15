c4aff1c8b3fb497ab780deab959218eaf68ae43f
teacher <teacher@khestep.edu>
2021-04-15 19:59:18 +0300

    python lesson1

---
+++ python/lesson1/1.py
@@ -0,0 +1,65 @@
+# print("Hello World", end="!!!")
+
+# print("Hello World", "John", "Alex", sep="-", end=".")
+
+# print("Hello" + 123)
+
+# print(10, 2, -1)
+# print(0x10)
+# print(12)
+
+# print(0.0000000000000001)
+# print(1e-16)
+
+# print("\"Hello World\"")
+
+# print(True > False, False == True)
+
+# print(None)
+
+# Operators + - * / // % **
+
+# print(2 + 2)
+# print(5 - 2)
+# print(6 * 2)
+# print(6 / 3)
+# print(5 // 2)
+# print(5 % 2)
+# print(2 ** 8)
+
+# print(2 + 8 * 2)
+# print(9 % 6 % 2)
+# print(6 * 5 * 9)
+
+#Variables
+
+# num1 = 9
+# num2 = 12
+
+# print(num1 + num2)
+
+# num1 = num1 + 9
+# print(num1)
+
+# a = 4.
+# b = 3.
+# print((a ** 2 + b ** 2)**.5)
+
+# age = 12
+# print(_age)
+
+
+# number = 10
+# number **= 3
+# print(number)
+
+# greeting = input("Enter something useful...\n")
+# print(2 + int(greeting))
+
+# a =  input("Enter catet1...\n")
+# b = input("Enter catet2...\n")
+# print((int(a) ** 2 + int(b) ** 2)**.5)
+
+# a = "James"
+# b = "James" * -5
+# print(b)
