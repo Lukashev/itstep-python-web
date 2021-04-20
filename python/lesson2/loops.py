82754e165974fd6dc482b5f1f6c657c95cb4d582
teacher <teacher@khestep.edu>
2021-04-20 20:58:48 +0300

    update

---
+++ python/lesson2/loops.py
@@ -0,0 +1,35 @@
+# while, for
+
+# print( range(2, 10, 3))
+
+# for c in range(10, 0, -20):
+#     print(c)
+
+# for c in range(10, 0, -1):
+#     if c == 5:
+#         break
+#     print(c)    
+
+# for c in range(10, 0, -1):
+#     if c == 5:
+#         continue
+#     print(c)    
+
+# for i in range(1, 10):
+#     for j in range(1, 10):
+#         print(i * j, end = "\t")
+#     print("\n")    
+
+# for c in range(10, 0, -1):
+#     print(c)
+# else:
+#     print("Hello World")    
+
+# result = 1
+# counter = 3
+
+# while counter:
+#     result *= counter
+#     counter -= 1 
+
+# print(result)
